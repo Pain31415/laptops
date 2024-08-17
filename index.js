8424da50a0fcca978ffe0455f38c8bdb658274d2
Chernii Vladyslav Ihorovych <Cherniy412@ukr.net>
2024-08-17 15:47:46 +0300

    up

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+import { addHeader } from "./src/js/addHeader.js";
+import { addToCart, cart } from "./src/js/cart.js";
 import { laptops } from "./src/js/db.js";
 import { filterGoods } from "./src/js/filter.js";
+import { closeCart, openCart } from "./src/js/open_close_cart.js";
 import { renderGoods } from "./src/js/renderGoods.js";
 import { renderSelect } from "./src/js/renderSelect.js";
 
+addHeader()
+openCart()
+closeCart()
 document.addEventListener("DOMContentLoaded", () => {
     renderGoods(laptops);
 
@@ -42,4 +48,14 @@ document.addEventListener("DOMContentLoaded", () => {
             document.getElementById("priceFilter").value;
         filterGoods(laptops);
     });
+
+    document.querySelector('.goods_container').addEventListener('click', (e) => {
+        if (e.target.classList.contains('buyBtn')) {
+            const laptopName = e.target.closest('.card').querySelector('h3').textContent
+            console.log(laptopName);
+            const laptop = laptops.find(product => product.name === laptopName)
+            console.log(laptop);
+            addToCart.call(cart, laptop)
+        }
+    })
 });
